5192ce49896636a274ad88230e500e28926fe376
HimanshuTiwariPlum <ht.28792@gmail.com>
2018-01-21 01:57:34 +0530

    solution problem 1

--- problem1/himanshu.rb
+++ problem1/himanshu.rb
@@ -1,2 +1,17 @@
 class Abc
+	def triangle(n)
+		if n > 0
+			actual = 0
+			for i in (1..n) do
+				# puts i
+				actual = (actual + i)
+			end
+			puts actual
+		else
+			puts 0
+		end
+	end
 end
+
+abc= Abc.new
+abc.triangle(5)
